b7d5bff500df8f2430bbf56f27a6c95fed430ad9
Issa.Mwakio <issamwakio67@gmail.com>
2026-06-28 17:23:38 +0300

    fix: allow vercel domain in backend CORS

--- server/main.py
+++ server/main.py
@@ -16,6 +16,7 @@ app = FastAPI(title="Daraja LMS API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "http://192.168.0.104:3000"],
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
